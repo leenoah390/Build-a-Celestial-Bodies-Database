9386709475318f4cf8f9b48e40bf4756438947d5
Noah Lee <138251680+leenoah390@users.noreply.github.com>
2024-06-07 21:14:57 -0400

    salon sh new

--- salon.sh
+++ salon.sh
@@ -5,13 +5,14 @@ echo -e "\n~~~~~ MY SALON ~~~~~\n"
 echo -e "Welcome to My Salon, how can I help you?\n" 
 
 MAIN_MENU() {
+  # Display the services
   ALL_SERVICES=$($PSQL "SELECT * FROM services")
-
   echo "$ALL_SERVICES" | while read SID BAR SNAME
   do
     echo "$SID) $SNAME"
   done
 
+  # What do they want?
   read SERVICE_ID_SELECTED
 
   # Check if input is viable
@@ -28,8 +29,8 @@ BOOK_MENU() {
 echo -e "\nWhat's your phone number?"
 read CUSTOMER_PHONE
 # Check if number exists already, and collect customer_id
-CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
-if [[ -z $CUSTOMER_ID ]]
+CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'" | sed -E 's/^ //g')
+if [[ -z $CUSTOMER_NAME ]]
 # If customer does not exist
   then
   echo -e "\nI don't have a record for that phone number, what's your name?"
@@ -38,14 +39,15 @@ if [[ -z $CUSTOMER_ID ]]
   INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(name , phone) VALUES ('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
 fi
 
-CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE customer_id=$CUSTOMER_ID")
+# Get customer ID from customers
+CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
 
 # Get the time
-echo -e "\nWhat time would you like your cut, Fabio?"
+echo -e "\nWhat time would you like your cut,$CUSTOMER_NAME?"
 read SERVICE_TIME
 # Output the sentence
-CHOICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-echo -e "\nI have put you down for a$CHOICE at $SERVICE_TIME,$CUSTOMER_NAME."
+CHOICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED" | sed -E 's/^ //g')
+echo -e "\nI have put you down for a $CHOICE at $SERVICE_TIME, $CUSTOMER_NAME."
 # Insert the data to appointments table
 INSERT_APP=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
 }
